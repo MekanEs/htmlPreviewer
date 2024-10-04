bd1715068292e9f549b7bba62eff882d6746641c
MekanEs <Mekan.es1997@gmail.com>
2024-10-04 13:46:13 +0300

    remove unused functions

--- src/editor-ex/index.ts
+++ src/editor-ex/index.ts
@@ -1,21 +1,12 @@
 import { Monaco } from '@monaco-editor/react';
 import {
-  // AutoCloseTag,
-  // CssColorInHtml,
   CssSuggestInHtml,
-  CssFoldingRangeInHtml,
-  CssHighlightInHtml,
-  CssSymbolInHtml,
-
 } from './html';
 import { tryInitMonaco } from './monaco';
 
 export function MonacoEx(monacoInstance: Monaco) {
   if (!tryInitMonaco(monacoInstance)) return;
-  CssSymbolInHtml();
+
   CssSuggestInHtml();
-  CssFoldingRangeInHtml();
-  CssHighlightInHtml();
-  // CssColorInHtml()
-  // AutoCloseTag();
+
 }
