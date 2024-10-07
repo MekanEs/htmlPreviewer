613887d30aca8379d02ced30e9340c3c0383b3d1
MekanEs <Mekan.es1997@gmail.com>
2024-10-07 12:52:58 +0300

    refactor: remove unused files

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,7 @@
 export {useRegMatcher} from './regMatcher';
-
 export { toggleImages } from './toggleFrameBorder';
-
 export { themeSwitcher } from './themeLoader';
-
 export { addRule } from './linter';
-
 export { addDataAttribute } from './DataAttributeAdder';
 export { createRange } from './createRange';
 export { HTMLOptionsSetter } from './htmlOptionsSetter';
