e28b74671677cc186abbbe3cbec357fc7f5529ec
MekanEs <Mekan.es1997@gmail.com>
2025-02-24 17:55:19 +0300

    add fixed tagpair rule but not adding it

--- src/constants/htmlHintRules.ts
+++ src/constants/htmlHintRules.ts
@@ -130,6 +130,59 @@ export const titleRequire: Rule = {
     },
 };
 
+export const tagPairCustom: Rule = {
+    id: 'tag-pair-custom',
+    description: 'Tag must be paired.',
+    init(parser, reporter) {
+        const stack: { tagName: string, line: number, raw: string }[] = [];
+        const mapEmptyTags = parser.makeMap('area,base,basefont,br,col,frame,hr,img,input,isindex,link,meta,param,embed,track,command,source,keygen,wbr');
+        parser.addListener('tagstart', (event) => {
+            const tagName = event.tagName.toLowerCase();
+            if (mapEmptyTags[tagName] === undefined && !event.close) {
+                stack.push({
+                    tagName: tagName,
+                    line: event.line,
+                    raw: event.raw,
+                });
+            }
+        });
+        parser.addListener('tagend', (event) => {
+            const tagName = event.tagName.toLowerCase();
+            let pos;
+            for (pos = stack.length - 1; pos >= 0; pos--) {
+                if (stack[pos].tagName === tagName) {
+                    break;
+                }
+            }
+            if (pos >= 0) {
+                const arrTags = [];
+                for (let i = stack.length - 1; i > pos; i--) {
+                    arrTags.push(`</${stack[i].tagName}>`);
+                }
+                if (arrTags.length > 0) {
+                    const lastEvent = stack[stack.length - 1];
+                    reporter.error(`Tag must be paired, missing: [ ${arrTags.join('')} ], start tag match failed [ ${lastEvent.raw} ] on line ${lastEvent.line}.`, lastEvent.line, event.col, this, event.raw);
+                    // here
+                }
+                stack.length = pos;
+            }
+            else {
+                reporter.error(`Tag must be paired, no start tag: [ ${event.raw} ]`, event.line, event.col, this, event.raw);
+            }
+        });
+        parser.addListener('end', (event) => {
+            const arrTags = [];
+            for (let i = stack.length - 1; i >= 0; i--) {
+                arrTags.push(`</${stack[i].tagName}>`);
+            }
+            if (arrTags.length > 0) {
+                const lastEvent = stack[stack.length - 1];
+                reporter.error(`Tag must be paired, missing: [ ${arrTags.join('')} ], open tag match failed [ ${lastEvent.raw} ] on line ${lastEvent.line}.`, event.line, event.col, this, '');
+            }
+        });
+    },
+};
+
 // const styleProperty: Rule = {
 //   id: 'style-property:value',
 //   description: 'every property should have value',
