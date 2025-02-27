213e6756b04ed3e4d1e350aa505622a0b7204e4a
MekanEs <Mekan.es1997@gmail.com>
2025-02-27 22:13:26 +0300

    add beforeUnload alert

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,23 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import styles from './Layout.module.scss';
 import { Outlet } from 'react-router-dom';
 import classNames from 'classnames';
 import { MonacoEx } from '../../editor-ex';
 import { monaco } from '../../constants';
-interface LayoutProps {}
+interface LayoutProps { }
 
 export const Layout: FC<LayoutProps> = () => {
+  useEffect(() => {
+    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+      event.preventDefault();
+      event.returnValue = "Вы обновляете страницу, прогресс не сохранён!";
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
   MonacoEx(monaco);
   return (
     <div className={classNames(styles.Layout)}>
