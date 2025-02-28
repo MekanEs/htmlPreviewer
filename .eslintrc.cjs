0ceff36bbe402ecb9c5aa55dbeab15af7c540eed
MekanEs <Mekan.es1997@gmail.com>
2025-02-28 15:12:41 +0300

    add linting and formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,12 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked', // stricter type checking
+    'plugin:@typescript-eslint/stylistic-type-checked',  // style rules for TypeScript
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',  // Add Prettier integration
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,5 +18,20 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'react/prop-types': 'off', // Since you're using TypeScript
+     'react/react-in-jsx-scope': 'off',
+     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+     '@typescript-eslint/explicit-function-return-type': 'off',
+     '@typescript-eslint/explicit-module-boundary-types': 'off',
+     '@typescript-eslint/no-explicit-any': 'warn',
+     'no-console': ['warn', { allow: ['warn', 'error'] }],
+     "import/order": [
+         "error",
+         {
+           "groups": ["builtin", "external", "internal", "parent", "sibling", "index"],
+           "newlines-between": "always",
+           "alphabetize": { "order": "asc" }
+         }
+       ]
   },
 }
