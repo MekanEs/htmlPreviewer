8c85fba5197a6fed133a4fd5718ba4f455f7d8d1
MekanEs <Mekan.es1997@gmail.com>
2025-03-09 13:43:16 +0300

    eslint conf

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,47 +4,44 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended-type-checked', // stricter type checking
-    'plugin:@typescript-eslint/stylistic-type-checked',  // style rules for TypeScript
+    'plugin:@typescript-eslint/stylistic-type-checked', // style rules for TypeScript
     'plugin:react-hooks/recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:prettier/recommended',  // Add Prettier integration
+    'plugin:prettier/recommended', // Add Prettier integration
   ],
   settings: {
     react: {
       version: 'detect',
     },
   },
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['react-refresh',"import"],
+  plugins: ['react-refresh', 'import'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'react/prop-types': 'off', // Since you're using TypeScript
-     'react/react-in-jsx-scope': 'off',
-     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-     '@typescript-eslint/explicit-function-return-type': 'off',
-     '@typescript-eslint/explicit-module-boundary-types': 'off',
-     "@typescript-eslint/require-await": "off",
-     "@typescript-eslint/no-floating-promises": "off",
-     "@typescript-eslint/restrict-template-expressions": "off",
-     '@typescript-eslint/no-explicit-any': 'warn',
-     'no-console': ['off'],
-     
-     "import/order": [
-         "error",
-         {
-           "groups": ["builtin", "external", "internal", "parent", "sibling", "index"],
-           "newlines-between": "always",
-           "alphabetize": { "order": "asc" }
-         }
-       ]
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/require-await': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': ['off'],
+
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc' },
+      },
+    ],
   },
-}
+};
